98be8bf4ced63ce8ee4efd5d167c9a10c136047b
M.Shahzaib <mohdshahzaib24@gmail.com>
2020-12-17 01:12:33 +0500

    added sw file

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import {ApolloProvider,ApolloClient, InMemoryCache} from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 const client = new ApolloClient({
   uri: 'https://spacexdata.herokuapp.com/graphql',
@@ -23,3 +24,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
